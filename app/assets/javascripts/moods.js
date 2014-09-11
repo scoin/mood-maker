d89be4f5cd0a0ebaf418a5e198189563a0dcbeb3
scoin <greg.piccolo@gmail.com>
2014-09-11 18:28:13 -0400

    getting and editing z-index working

--- app/assets/javascripts/moods.js
+++ app/assets/javascripts/moods.js
@@ -1,4 +1,5 @@
 var image = '';
+var selected = '';
 
 $(document).ready(function(){
 
@@ -9,12 +10,23 @@ $(document).ready(function(){
 		e.preventDefault();
 		$.post('/addimage', $('#addImage').serialize(), function(){
 			image = $('#imageURL').val();
+			$('#imageURL').val('');
 		})
 			.done(function(data){
 				$('.page').append("<div class='imageDiv'><img src='" + image + "' class='image'></div>")
 				$(".imageDiv").draggable({ containment: ".page", scroll: true });
 				$('.image').resizable({containment: ".page", aspectRatio: true});
-				$('#imageURL').val('');
-			})
+			});
+	});
+
+	$('.page').on('click', '.imageDiv', function(){
+		selected = this;
+		$('#z-index').val($(this).zIndex());
+	})
+
+	$('#z-index').keyup(function(k){
+		if(selected != undefined){
+			$(selected).zIndex($('#z-index').val());
+		}
 	})
 })
