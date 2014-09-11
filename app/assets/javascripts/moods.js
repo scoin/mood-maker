21335b6d111ab2b4ad272cfbeef8d192e731e455
scoin <greg.piccolo@gmail.com>
2014-09-11 18:04:10 -0400

    add multiple images on frontend, preparation for persistence

--- app/assets/javascripts/moods.js
+++ app/assets/javascripts/moods.js
@@ -1,5 +1,20 @@
+var image = '';
+
 $(document).ready(function(){
 
 	$(".imageDiv").draggable({ containment: ".page", scroll: true });
-	$('#image').resizable({containment: ".page", aspectRatio: true});
+	$('.image').resizable({containment: ".page", aspectRatio: true});
+
+	$('#addImage').submit(function(e){
+		e.preventDefault();
+		$.post('/addimage', $('#addImage').serialize(), function(){
+			image = $('#imageURL').val();
+		})
+			.done(function(data){
+				$('.page').append("<div class='imageDiv'><img src='" + image + "' class='image'></div>")
+				$(".imageDiv").draggable({ containment: ".page", scroll: true });
+				$('.image').resizable({containment: ".page", aspectRatio: true});
+				$('#imageURL').val('');
+			})
+	})
 })
--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,5 +1,10 @@
 class MoodsController < ApplicationController
 	def index
-		
+
+	end
+
+	def addimage
+		puts params
+		render plain: "YES"
 	end
 end
